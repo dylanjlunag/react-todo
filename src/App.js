54f6960d73242414c99d4449d5b4df5b30ee483a
Dylan Luna <dylanjlunag@gmail.com>
2022-01-09 21:47:10 -0600

    feat: todo context implemented in app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import TodoPage from '@components/TodoPage';
+import { TodoProvider } from '@context/TodoContext';
+
 export default function App() {
-  return <h1>Let&apos;s Code</h1>;
+  return (
+    <TodoProvider>
+      <TodoPage />
+    </TodoProvider>
+  );
 }
