6ab442a28ad6d20649d61633ab47b85bda9de5fe
Dylan Luna <dylanjlunag@gmail.com>
2022-02-02 19:04:21 -0600

    feat(components): now select consume todo context

--- src/components/TodoSelect/index.jsx
+++ src/components/TodoSelect/index.jsx
@@ -1,11 +1,20 @@
+import { useTodo } from '@context/TodoContext';
 import styles from './styles.module.scss';
 
 export default function TodoSelect() {
+  const { filters, selectFilter } = useTodo();
+
+  const handleChange = (e) => {
+    selectFilter(e.target.value);
+  };
+
   return (
-    <select className={styles.select}>
-      <option value="all">All</option>
-      <option value="completed">Completed</option>
-      <option value="uncompleted">Uncompleted</option>
+    <select className={styles.select} onChange={handleChange}>
+      {filters.map((filter) => (
+        <option key={filter.id} value={filter.id}>
+          {filter.name}
+        </option>
+      ))}
     </select>
   );
 }
